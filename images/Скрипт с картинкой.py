e0a799d1f5379a34beb9f9af598ded858bc76dfd
Anna-Vas <32746838+Anna-Vas@users.noreply.github.com>
2019-05-26 01:04:55 +0300

    Add files via upload

---
+++ images/Скрипт с картинкой.py
@@ -0,0 +1,14 @@
+from PIL import Image
+
+im = Image.open('ironman.jpg')
+im.show()
+
+pixels = im.load()
+
+for i in range(im.width):
+    for j in range(im.height):
+        r, g, b = pixels[i, j]
+        r = min(r + 100, 255)
+        pixels[i, j] = (r, g, b)
+
+im.show()
