78ff6b034ad24c46547c151a95cff38f361c4f17
Anna-Vas <32746838+Anna-Vas@users.noreply.github.com>
2020-01-25 22:48:11 +0300

    Added comments

--- bots/bot_goes_to_the_corner.py
+++ bots/bot_goes_to_the_corner.py
@@ -2,12 +2,15 @@ import random
  
 def make_choice(x,y,field):
     
+    # Define size of the field  
     x_size = len(field)
     y_size = len(field[0])
     
-    if x<x_size-1:
+    # Bot starts moving towards the right bottom corner
+    if x < x_size - 1:
         return "go_right"
-    if y<y_size-1:
+    if y < y_size - 1:
         return "go_down"
     
+    # Bot starts randomly shooting when he'd reached the corner
     return random.choice(["fire_up", "fire_left"])
