a9a2d9082349cfcdc8c91bbb99e7f6bf852904e8
Anna-Vas <32746838+Anna-Vas@users.noreply.github.com>
2020-01-25 23:20:29 +0300

    Update and rename Скрипт с картинкой 2.py to image_black_and_white.py

--- images/image_black_and_white.py
+++ images/image_black_and_white.py
@@ -1,14 +1,17 @@
 from PIL import Image
 
+# Open image and load pixels
 im = Image.open('ironman.jpg')
 im.show()
 
 pixels = im.load()
 
+# Black and white filter
 for i in range(im.width):
     for j in range(im.height):
         r, g, b = pixels[i, j]
         r = g = b
         pixels[i, j] = (r, g, b)
 
+# Show result
 im.show()
